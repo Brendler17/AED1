afddb028a52cd022dbb19eca4c6dfdef6767451f
Brendler17 <gmbrendler@inf.ufpel.edu.br>
2024-12-04 17:22:42 -0300

    added exercise 24

--- ProgramaçãoDescomplicada/Lista1-Variáveis/24/main.c
+++ ProgramaçãoDescomplicada/Lista1-Variáveis/24/main.c
@@ -4,7 +4,8 @@ int main() {
   float m2, ac;
 
   printf("Digite a área em metros quadrados: ");
-  scanf("%f", &m2), ac = m2 * 0.000247;
+  scanf("%f", &m2);
+  ac = m2 * 0.000247;
   printf("A área em acres é %f\n\n", ac);
 
   return 0;
