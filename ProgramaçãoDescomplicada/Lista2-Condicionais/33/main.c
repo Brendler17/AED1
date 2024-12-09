67df6f3a29b0b5d1d31137578f23183e5f53dc27
Brendler17 <gmbrendler@inf.ufpel.edu.br>
2024-12-09 17:29:33 -0300

    added exercise 33

--- ProgramaçãoDescomplicada/Lista2-Condicionais/33/main.c
+++ ProgramaçãoDescomplicada/Lista2-Condicionais/33/main.c
@@ -1,21 +1,37 @@
 #include <stdio.h>
 
 int main() {
-  float price;
+  float price, newPrice;
 
   printf("Digite o valor antigo do produto: ");
   scanf("%f", &price);
 
   if (price < 50.0) {
-    printf("O novo valor do produto aumentou 5%% e está R$%.2f\n\n", price + (price * 5) / 100);
+    newPrice = price + (price * 5) / 100;
   }
 
   if (price >= 50 && price <= 100) {
-    printf("O novo valor do produto aumentou 10%% e está R$%.2f\n\n", price + (price * 10) / 100);
+    newPrice = price + (price * 10) / 100;
   }
 
   if (price > 100) {
-    printf("O novo valor do produto aumentou 55%% e está R$%.2f\n\n", price + (price * 15) / 100);
+    newPrice = price + (price * 15) / 100;
+  }
+
+  if (newPrice < 80) {
+    printf("O novo preço do produto está BARATO e é R$%.2f\n\n", newPrice);
+  }
+
+  if (newPrice >= 80 && newPrice <= 120) {
+    printf("O novo preço do produto está NORMAL e é R$%.2f\n\n", newPrice);
+  }
+
+  if (newPrice > 120 && newPrice <= 200) {
+    printf("O novo preço do produto está CARO e é R$%.2f\n\n", newPrice);
+  }
+
+  if (newPrice > 200) {
+    printf("O novo preço do produto está MUITO CARO e é R$%.2f\n\n", newPrice);
   }
 
   return 0;
--- ProgramaçãoDescomplicada/Lista2-Condicionais/33/readme.md
+++ ProgramaçãoDescomplicada/Lista2-Condicionais/33/readme.md
@@ -1,5 +1,5 @@
 Um produto vai sofrer aumento de acordo com a tabela abaixo. 
-Leia o preço antigo, calcule e escreva o preço novo, e escreva uma mensagem em função do preço novo (de acordo com a segunda coluna).
+Leia o preço antigo, calcule e escreva o preço novo, e escreva uma mensagem em função do preço novo (de acordo com a segunda tabela).
 
 <table>
   <thead>
@@ -20,6 +20,33 @@ Leia o preço antigo, calcule e escreva o preço novo, e escreva uma mensagem em
     <tr>
       <td>acima de R$ 100</td>
       <td>15%</td>
+    </tr>
+  </tbody>
+</table>
+
+<table>
+  <thead>
+    <tr>
+      <th>Preço Novo</th>
+      <th>Mensagem</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>até R$ 80</td>
+      <td>Barato</td>
+    </tr>
+    <tr>
+      <td>entre R$ 80 e R$ 120 (inclusive)</td>
+      <td>Normal%</td>
+    </tr>
+    <tr>
+      <td>entre R$ 120 e R$ 200 (inclusive)</td>
+      <td>Caro</td>
+    </tr>
+    <tr>
+      <td>acima de R$ 200</td>
+      <td>Muito caro</td>
     </tr>
   </tbody>
 </table>
