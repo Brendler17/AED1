2698e91819bbfa374decc1d19721750bb33d0620
Brendler17 <gmbrendler@inf.ufpel.edu.br>
2025-01-11 18:13:28 -0300

    added exercise 9

---
+++ ProgramaçãoDescomplicada/Lista10-AlocaçãoDinâmica/09/main.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+  int* buffer = NULL;
+  int number, countElements = 1;
+
+  do {
+    printf("Digite um número: ");
+    scanf("%d", &number);
+
+    if (number > 0) {
+      buffer = (int*)realloc(buffer, countElements * sizeof(int));
+      if (buffer == NULL) {
+        printf("Erro ao alocar memória!");
+        return 1;
+      }
+
+      buffer[countElements - 1] = number;
+      countElements++;
+    }
+  } while (number > 0);
+
+  if (buffer) {
+    printf("\nO vetor digitado é: ");
+    for (int counter = 0; counter < countElements - 1; counter++) {
+      printf("%d ", buffer[counter]);
+    }
+  } else {
+    printf("\nO vetor está vazio.");
+  }
+
+  printf("\n\n");
+
+  free(buffer);
+
+  return 0;
+}
---
+++ ProgramaçãoDescomplicada/Lista10-AlocaçãoDinâmica/09/readme.md
@@ -0,0 +1,3 @@
+Faça um programa que leia uma quantidade qualquer de números armazenando-os na
+memória e pare a leitura quando o usuário entrar um número negativo. 
+Em seguida, imprima o vetor lido. Use a função REALLOC.
