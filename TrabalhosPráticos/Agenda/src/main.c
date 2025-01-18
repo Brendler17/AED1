67a37a1ce36550a199f7512533a75a4dc0536e7a
Brendler17 <gmbrendler@inf.ufpel.edu.br>
2025-01-18 14:52:44 -0300

    new person record logic created

--- TrabalhosPráticos/Agenda/src/main.c
+++ TrabalhosPráticos/Agenda/src/main.c
@@ -2,6 +2,43 @@
 #include <stdlib.h>
 #include <string.h>
 
+void updatePointers(void **pBuffer, void **userOption, void **counterPeoples, void **peopleLenght, void **peoplesBuffer, void **endBuffer, void **people) {
+  *userOption = *pBuffer;
+  *counterPeoples = *pBuffer + sizeof(int);
+  *peopleLenght = *pBuffer + sizeof(int) + sizeof(int);
+  *peoplesBuffer = *pBuffer + 2 * sizeof(int) + sizeof(size_t);
+  *endBuffer = *peoplesBuffer + *((int *)(*counterPeoples)) * (*((size_t *)*(peopleLenght)));
+  *people = *peoplesBuffer + (*((int *)*(counterPeoples)) - 1) * (*((size_t *)*(peopleLenght)));
+}
+
+int removeTrailingNewLine(char *string, void **pBuffer, void **counterPeoples, void **peopleLenght, void **userOption, void **peoplesBuffer, void **endBuffer, void **people) {
+  void *len;
+
+  void *tempBuffer = (void *)realloc(*pBuffer, 2 * sizeof(int) + sizeof(size_t) + (*((int *)*(counterPeoples))) * (*((size_t *)*(peopleLenght))) + sizeof(size_t));
+  if (tempBuffer == NULL) {
+    return 1;
+  }
+
+  *pBuffer = tempBuffer;
+  updatePointers(pBuffer, userOption, counterPeoples, peopleLenght, peoplesBuffer, endBuffer, people);
+
+  len = *endBuffer - sizeof(size_t);
+  *(size_t *)len = strlen(string);
+  if (*(size_t *)len > 0 && string[(*(size_t *)len) - 1] == '\n') {
+    string[(*(size_t *)len) - 1] = '\0';
+  }
+
+  tempBuffer = (void *)realloc(*pBuffer, 2 * sizeof(int) + sizeof(size_t) + (*((int *)*counterPeoples)) * (*((size_t *)*peopleLenght)));
+  if (tempBuffer == NULL) {
+    return 1;
+  }
+
+  *pBuffer = tempBuffer;
+  updatePointers(pBuffer, userOption, counterPeoples, peopleLenght, peoplesBuffer, endBuffer, people);
+
+  return 0;
+}
+
 void showMenu() {
   printf("\n-------------------- MENU --------------------\n");
   printf("1 - Adicionar Pessoa (Nome, Idade, E-Mail)\n");
@@ -13,7 +50,7 @@ void showMenu() {
 }
 
 int main(int argc, char const *argv[]) {
-  void *pBuffer, *endBuffer, *userOption, *counterPeoples, *peopleLenght;
+  void *pBuffer, *endBuffer, *tempBuffer, *userOption, *counterPeoples, *peopleLenght, *peoplesBuffer, *people;
 
   pBuffer = (void *)malloc(2 * sizeof(int) + sizeof(size_t));
   if (pBuffer == NULL) {
@@ -37,7 +74,40 @@ int main(int argc, char const *argv[]) {
 
     switch (*(int *)userOption) {
       case 1:
-        printf("Adicionar Pessoa");
+        (*(int *)counterPeoples)++;
+        tempBuffer = (void *)realloc(pBuffer, 2 * sizeof(int) + sizeof(size_t) + (*(int *)counterPeoples) * (*(size_t *)peopleLenght));
+        if (tempBuffer == NULL) {
+          printf("\nErro ao alocar memória!\n");
+          free(pBuffer);
+          return 1;
+        }
+
+        pBuffer = tempBuffer;
+        updatePointers(&pBuffer, &userOption, &counterPeoples, &peopleLenght, &peoplesBuffer, &endBuffer, &people);
+
+        system("clear");
+        printf("\n-------------- Adicionar Pessoa --------------\n");
+        printf("Informe o nome: ");
+        fgets(people, 50 * sizeof(char), stdin);
+        if (removeTrailingNewLine(people, &pBuffer, &counterPeoples, &peopleLenght, &userOption, &peoplesBuffer, &endBuffer, &people)) {
+          printf("\nErro ao alocar memória!\n");
+          free(pBuffer);
+          return 1;
+        }
+        printf("Informe a idade: ");
+        scanf("%d", (int *)(people + 50 * sizeof(char)));
+        getchar();
+        printf("Informe o e-mail: ");
+        fgets((people + 50 * sizeof(char) + sizeof(int)), 50 * sizeof(char), stdin);
+        if (removeTrailingNewLine((people + 50 * sizeof(char) + sizeof(int)), &pBuffer, &counterPeoples, &peopleLenght, &userOption, &peoplesBuffer, &endBuffer, &people)) {
+          printf("\nErro ao alocar memória!\n");
+          free(pBuffer);
+          return 1;
+        }
+
+        system("clear");
+        printf("\nRegistro adicionado com sucesso!\n");
+
         break;
       case 2:
         printf("Remover Pessoa");
