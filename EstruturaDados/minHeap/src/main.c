32951f990d0a8e32c18196eb851dcc40f8e1c720
Brendler17 <gmbrendler@inf.ufpel.edu.br>
2025-02-19 16:31:44 -0300

    created minHeap structure

---
+++ EstruturaDados/minHeap/src/main.c
@@ -0,0 +1,20 @@
+#include <math.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct {
+  int number;
+} Data;
+
+typedef struct Node {
+  int identifier;
+  Data data;
+  struct Node *pLeft;
+  struct Node *pRight;
+} Node;
+
+typedef struct {
+  Node *pRoot;
+  size_t nodeCounter;
+} minHeap;
