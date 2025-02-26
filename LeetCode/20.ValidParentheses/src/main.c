5be997849988e2bc58ca2514897adcd530531490
Brendler17 <gmbrendler@inf.ufpel.edu.br>
2025-02-26 16:45:49 -0300

    updated exercise 20

--- LeetCode/20.ValidParentheses/src/main.c
+++ LeetCode/20.ValidParentheses/src/main.c
@@ -5,51 +5,27 @@
 
 typedef struct {
   char* elements;
-  char* top;
-  char* base;
-  int numberOfElements;
+  int top;
+  int capacity;
 } Stack;
 
-bool reset(Stack** stack) {
-  *stack = (Stack*)malloc(sizeof(Stack));
-  if (*stack == NULL) {
+bool reset(Stack* stack, int capacity) {
+  stack->elements = (char*)malloc(capacity * sizeof(char));
+  if (stack->elements == NULL) {
     printf("\nError allocating memory!\n");
     return false;
   }
 
-  (*stack)->elements = NULL;
-  (*stack)->top = NULL;
-  (*stack)->base = NULL;
-  (*stack)->numberOfElements = 0;
-
+  stack->top = -1;
+  stack->capacity = capacity;
   return true;
 }
 
-bool empty(Stack* stack) { return stack->base == NULL; }
+bool empty(Stack* stack) { return stack->top == -1; }
 
-bool push(Stack* stack, char* bracket) {
-  if (stack->elements == NULL) {
-    char* elements = (char*)malloc(sizeof(char));
-    if (elements == NULL) {
-      printf("\nError allocating memory!\n");
-      return false;
-    }
-
-    stack->elements = elements;
-  } else {
-    char* elements = (char*)realloc(stack->elements, (stack->numberOfElements + 1) * sizeof(char));
-    if (elements == NULL) {
-      printf("\nError allocating memory!\n");
-      return false;
-    }
-
-    stack->elements = elements;
-  }
-
-  stack->elements[stack->numberOfElements] = *bracket;
-  stack->numberOfElements++;
-  stack->top = &stack->elements[stack->numberOfElements - 1];
-  stack->base = stack->elements;
+bool push(Stack* stack, char bracket) {
+  stack->top++;
+  stack->elements[stack->top] = bracket;
 
   return true;
 }
@@ -60,26 +36,8 @@ bool pop(Stack* stack, char* bracket) {
     return false;
   }
 
-  *bracket = stack->elements[stack->numberOfElements - 1];
-  stack->numberOfElements--;
-
-  if (stack->numberOfElements == 0) {
-    free(stack->elements);
-    stack->elements = NULL;
-    stack->top = NULL;
-    stack->base = NULL;
-    stack->numberOfElements = 0;
-    return true;
-  }
-
-  stack->elements = (char*)realloc(stack->elements, stack->numberOfElements * sizeof(char));
-  if (stack->elements == NULL) {
-    printf("\nError allocating memory!\n");
-    return false;
-  }
-
-  stack->top = &stack->elements[stack->numberOfElements - 1];
-  stack->base = stack->elements;
+  *bracket = stack->elements[stack->top];
+  stack->top--;
 
   return true;
 }
@@ -90,75 +48,50 @@ bool clear(Stack* stack) {
     stack->elements = NULL;
   }
 
-  stack->top = NULL;
-  stack->base = NULL;
-  stack->numberOfElements = 0;
+  stack->top = -1;
+  stack->capacity = 0;
 
   return true;
 }
 
 bool isValid(char* string) {
-  Stack* stack = NULL;
-  reset(&stack);
-
   size_t len = strlen(string);
+
+  if (len % 2 != 0) return false;
+
+  Stack stack;
+  if (!(reset(&stack, len))) return false;
+
   char bracket;
 
-  if (len % 2 != 0) {
-    return false;
-  }
-
   for (int counter = 0; counter < len; counter++) {
-    if (string[counter] == '(' || string[counter] == '[' || string[counter] == '{') {
-      push(stack, &string[counter]);
-    } else if (string[counter] == ')') {
-      if (pop(stack, &bracket)) {
-        if (bracket != '(') {
-          return false;
-        }
-      } else {
+    char c = string[counter];
+
+    if (c == '(' || c == '[' || c == '{') {
+      push(&stack, c);
+    } else {
+      if (!pop(&stack, &bracket)) {
+        clear(&stack);
         return false;
       }
-    } else if (string[counter] == ']') {
-      if (pop(stack, &bracket)) {
-        if (bracket != '[') {
-          return false;
-        }
-      } else {
-        return false;
-      }
-    } else if (string[counter] == '}') {
-      if (pop(stack, &bracket)) {
-        if (bracket != '{') {
-          return false;
-        }
-      } else {
+
+      if ((c == ')' && bracket != '(') || (c == ']' && bracket != '[') || (c == '}' && bracket != '{')) {
+        clear(&stack);
         return false;
       }
     }
   }
 
-  if (!empty(stack)) {
-    return false;
-  }
+  bool valid = empty(&stack);
 
-  clear(stack);
-  free(stack);
-  return true;
+  clear(&stack);
+  return valid;
 }
 
 int main() {
-  char string1[] = {"()"};
-  char string2[] = {"()[]{}"};
-  char string3[] = {"(]"};
-  char string4[] = {"([])"};
-  char string5[] = {"(("};
-
-  isValid(string1) ? printf("True\n") : printf("False\n");
-  isValid(string2) ? printf("True\n") : printf("False\n");
-  isValid(string3) ? printf("True\n") : printf("False\n");
-  isValid(string4) ? printf("True\n") : printf("False\n");
-  isValid(string5) ? printf("True\n") : printf("False\n");
-
+  char* testCases[] = {"()", "()[]{}", "(]", "([])", "(("};
+  for (int counter = 0; counter < 5; counter++) {
+    isValid(testCases[counter]) ? printf("True\n") : printf("False\n");
+  }
   return 0;
 }
